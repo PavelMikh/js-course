e1e313ce6621f7ff3787249cb0b08c5050686dd6
PavelMikh <pamix1985@gmail.com>
2021-11-09 02:23:36 +0700

    create base rollup config

--- project_builders/index.js
+++ project_builders/index.js
@@ -1,1 +1,14 @@
+import './index.css';
+
 console.log('hello world');
+
+const title = document.createElement('h1');
+title.textContent = 'I love JavaScript!';
+title.classList.add('title');
+
+const img = document.createElement('img');
+img.src = './assets/js.png';
+img.classList.add('image');
+
+document.body.append(title);
+document.body.append(img);
